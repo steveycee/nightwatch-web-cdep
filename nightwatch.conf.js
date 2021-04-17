9d907f4f0f22b7589399efaef0f78b0865a758e3
steveycee <stevey.cee@gmail.com>
2021-04-17 13:11:23 +0100

    feat: added VRT
    Added VRT, run test, works, hot damn

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,7 +1,30 @@
 module.exports = {
   src_folders: ["tests"],
   page_objects_path: ["page_objects"],
-  custom_commands_path: ["commands"],
+  // custom_commands_path: ["node_modules/nightwatch-vrt/commands", "commands"],
+  custom_commands_path: ["node_modules/nightwatch-vrt/commands"],
+  custom_assertions_path: ["node_modules/nightwatch-vrt/assertions"],
+
+  // VRT
+
+  default: {
+    globals: {
+      visual_regression_settings: {
+        generate_screenshot_path: generateScreenshotFilePath,
+        latest_screenshots_path: "vrt/latest",
+        latest_suffix: "",
+        baseline_screenshots_path: "vrt/baseline",
+        baseline_suffix: "",
+        diff_screenshots_path: "vrt/diff",
+        diff_suffix: "",
+        threshold: 0,
+        prompt: false,
+        always_save_diff_screenshot: false,
+      },
+    },
+  },
+
+  // Basic
 
   webdriver: {
     start_process: true,
@@ -18,3 +41,10 @@ module.exports = {
     },
   },
 };
+
+function generateScreenshotFilePath(nightwatchClient, basePath, fileName) {
+  const moduleName = nightwatchClient.currentTest.module,
+    testName = nightwatchClient.currentTest.name;
+
+  return path.join(process.cwd(), basePath, moduleName, testName, fileName);
+}
