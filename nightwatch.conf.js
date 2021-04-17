0cbc52d2474d5556f008343c17f9a2e594a957d8
steveycee <stevey.cee@gmail.com>
2021-04-17 12:19:39 +0100

    chore: updated config

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,23 +1,20 @@
 module.exports = {
-    // src_folders: ['specs/beta_specs', 'specs/live_specs'],
-    src_folders: ['specs/beta_specs'],
-    page_objects_path: ['page_objects'],
-    custom_commands_path: ['commands'],
-  
-    webdriver: {
-      start_process: true,
-      port: 9515,
-      server_path: require('chromedriver').path,
-      "cli_args": [
-        "--verbose"
-      ],
+  src_folders: ["tests"],
+  page_objects_path: ["page_objects"],
+  custom_commands_path: ["commands"],
+
+  webdriver: {
+    start_process: true,
+    port: 9515,
+    server_path: require("chromedriver").path,
+    cli_args: ["--verbose"],
+  },
+
+  test_settings: {
+    default: {
+      desiredCapabilities: {
+        browserName: "chrome",
+      },
     },
-  
-    test_settings: {
-      default: {
-        desiredCapabilities : {
-          browserName : 'chrome',
-        }
-      }
-    }
-  };
+  },
+};
